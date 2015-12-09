d8e61d9ab2113aeb7adf3daab02754d4ac3b081c
Corey Sewell <corey@gurudigital.nz>
2015-12-09 21:09:55 +1300

    Cleaned up SSGuru_Requirements_Backend
    Added UploadFieldFactory

---
+++ code/Helpers/UploadFieldFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+class UploadFieldFactory {
+
+    /**
+     * Return a new UploadField instance, preconfiguger to only allow one image
+     *
+     * @param string $name The internal field name, passed to forms.
+     * @param string $title The field label.
+     * @param SS_List $items If no items are defined, the field will try to auto-detect an existing relation on
+     *                       @link $record}, with the same name as the field name.
+     * @param Form $form Reference to the container form
+     */
+    public static function SingleImage($name, $title = null, SS_List $dataList = null) {
+        $field = UploadField::create($name, $title, $dataList)
+                ->setAllowedFileCategories('image')
+                ->setConfig('allowedMaxFileNumber', 1);
+        return $field;
+    }
+
+}
--- code/View/SSGuru_Requirements_Backend.php
+++ code/View/SSGuru_Requirements_Backend.php
@@ -2,98 +2,85 @@
 
 class SSGuru_Requirements_Backend extends Requirements_Backend {
 
-	public function javascript($file) {
-		if (!$this->isURL($file)) {
-			if (!Director::fileExists($file)) {
-				$file = $this->findRequirementFile($file, "js");
-			}
-		}
-		return parent::javascript($file);
-	}
+    public function javascript($file) {
+        return parent::javascript($this->findRequirementFile($file, "js"));
+    }
 
-	public function css($file, $media = null) {
-		if (!$this->isURL($file)) {
-			if (!Director::fileExists($file)) {
-				$file = $this->findRequirementFile($file, "css");
-			}
-		}
-		return parent::css($file, $media);
-	}
+    public function css($file, $media = null) {
+        return parent::css($this->findRequirementFile($file, "css"), $media);
+    }
 
-//	public function css1($fileOrUrl) {
-//		if (!$this->isURL($fileOrUrl)) {
-//			if (!Director::fileExists($fileOrUrl)) {
-//
-//			}
-//		}
-//		return parent::css($fileOrUrl);
-//	}
+    public function themedCSS($name, $module = null, $media = null) {
+        return parent::themedCSS($name, $module, $media);
+    }
 
-	public function themedCSS($name, $module = null, $media = null) {
-		return parent::themedCSS($name, $module, $media);
-	}
+    protected function isURL($fileOrUrl) {
+        return preg_match('{^//|http[s]?}', $fileOrUrl);
+    }
 
-	protected function isURL($fileOrUrl) {
-		return preg_match('{^//|http[s]?}', $fileOrUrl);
-	}
+    protected function findRequirementFile($fileToFind, $ext) {
+        $result = $fileToFind;
+        if (!$this->isURL($result) && !Director::fileExists($result)) {
+            $lookedIn   = array();
+            $folderList = $this->getFolderList($ext);
+            $fileList   = $this->getFileList($fileToFind, $ext);
+            foreach ($folderList as $folder) {
+                foreach ($fileList as $file) {
+                    $lookedIn[] = $folder . '/' . $file;
+                    if (Director::fileExists($folder . '/' . $file)) {
+                        $result = $folder . '/' . $file;
+                        break 2;
+                    }
+                }
+            }
+            if (Config::inst()->forClass(get_called_class())->get("WarnOnNotFound") && !Director::fileExists($result)) {
+                Debug::message("Requirement file \"" . $fileToFind . "\" not found");
+                Debug::dump($lookedIn);
+            }
+        }
+        return $result;
+    }
 
-	protected function findRequirementFile($fileToFind, $ext) {
-		$result		 = $fileToFind;
-		$folderList	 = $this->getFolderList($ext);
-		$fileList	 = $this->getFileList($fileToFind, $ext);
-		foreach ($folderList as $folder) {
-			foreach ($fileList as $file) {
-				if (Director::fileExists($folder . '/' . $file)) {
-					$result = $folder . '/' . $file;
-					break 2;
-				}
-			}
-		}
-		return $result;
-	}
+    protected function getFileList($fileToFind, $ext) {
+        $cleanExt  = "." . ltrim($ext, ".");
+        $fileNoExt = preg_replace("/(.min)?" . $cleanExt . "$/", "", $fileToFind);
+        // If dev mode .min has less priority
+        return Director::isDev() ?
+                array(
+            $fileNoExt . $cleanExt,
+            $fileNoExt . ".min" . $cleanExt
+                ) :
+                array(
+            $fileNoExt . ".min" . $cleanExt,
+            $fileNoExt . $cleanExt,
+        );
+    }
 
-	protected function getFileList($fileToFind, $ext) {
-		$cleanExt	 = "." . ltrim($ext, ".");
-		$fileNoExt	 = preg_replace("/(.min)?" . $cleanExt . "$/", "", $fileToFind);
-		// If dev mode .min has less priority
-		return Director::isDev() ?
-				array(
-			$fileNoExt . $cleanExt,
-			$fileNoExt . ".min" . $cleanExt
-				) :
-				array(
-			$fileNoExt . ".min" . $cleanExt,
-			$fileNoExt . $cleanExt,
-		);
-	}
+    protected function getFolderList($ext) {
+        $subFolders = array();
+        if (strrpos($ext, "css", -strlen($ext)) !== false) {
+            $subFolders[] = "css";
+        } else if (strrpos($ext, "js", -strlen($ext)) !== false) {
+            $subFolders[] = "javascript";
+            $subFolders[] = "js";
+        }
 
-	protected function getFolderList($ext) {
-		$subFolders = array();
-		if (strrpos($ext, "css", -strlen($ext)) !== false) {
-			$subFolders[] = "css";
-		} else if (strrpos($ext, "js", -strlen($ext)) !== false) {
-			$subFolders[]	 = "javascript";
-			$subFolders[]	 = "js";
-		}
+        $baseFolders = array(
+            SSViewer::get_theme_folder(),
+            project()
+        );
 
-		$baseFolders = array(
-			SSViewer::get_theme_folder(),
-			project()
-		);
+        $result = array();
+        foreach ($baseFolders as $baseFolder) {
+            foreach ($subFolders as $subFolder) {
+                $result[] = $baseFolder . '/' . $subFolder;
+            }
+        }
+        return $result;
+    }
 
-		$result = array();
-		foreach ($baseFolders as $baseFolder) {
-			foreach ($subFolders as $subFolder) {
-				$result[] = $baseFolder . '/' . $subFolder;
-			}
-		}
-
-		return $result;
-	}
-
-	protected function path_for_file($fileOrUrl) {
-//		Debug::message($fileOrUrl . " " . (Controller::curr()->IsCMS() ? "Admin" : "FrontEnd"));
-		return parent::path_for_file($fileOrUrl);
-	}
+    protected function path_for_file($fileOrUrl) {
+        return parent::path_for_file($fileOrUrl);
+    }
 
 }
